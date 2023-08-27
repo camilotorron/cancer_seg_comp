6d31c4de72a8ad4fe0252b03996a06cac82fea53
camilotorron <camilo_torron@hotmail.com>
2023-08-28 00:19:46 +0200

    added brain data

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -11,9 +11,15 @@ class Environment(BaseSettings):
     IMAGES_PATH: str = os.getenv("IMAGES_PATH")
     BREAST_US: str = "breast_ultrasound_images_dataset/Dataset_BUSI_with_GT_proc"
     YOLO_DATASET_OUTPUT: str = str(Path("data/yolo_ouput_dataset").resolve())
-    labels_dict = {"benign": 0, "normal": 1, "malignant": 2}
+    labels_dict = {
+        "benign": 0,
+        "malignant": 1,
+        "normal": 2,
+    }
     DEF_YOLO_DET_THRESHOLD = 0.5
     FINETUNED_YOLO = "models/finetuned_yolo.pt"
+    BRAIN_DATA = "brain_MRI_segmentation/kaggle_3m_png"
+    # BRAIN_DATA = "brain_MRI_segmentation/kaggle_3m"
 
 
 env = Environment()

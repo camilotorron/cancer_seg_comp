146ac22c9ea801a54915334a0bc5c81b131afe0a
camilotorron <camilo_torron@hotmail.com>
2023-08-28 00:19:37 +0200

    ADded callback and parameters

--- src/techniques/classification/yolo.py
+++ src/techniques/classification/yolo.py
@@ -10,9 +10,17 @@ class Yolo:
 
     def __init__(self):
         self.model = YOLO("models/yolov8s.pt")
+        # self.model.add_callback("on_train_start", self.freeze_layer)
 
-    def train(self, data: str, epochs: int = 30, imgsz: int = 640):
-        self.model.train(data=data, epochs=epochs, imgsz=imgsz, device=0)
+    def train(self, data: str, epochs: int = 30, imgsz: int = 256):
+        self.model.train(
+            data=data,
+            epochs=epochs,
+            imgsz=imgsz,
+            device=0,
+            patience=5,
+            optimizer="Adam",
+        )
 
     def inference(self, image: str, checkout_path=env.FINETUNED_YOLO):
         self.model = YOLO(checkout_path)
@@ -31,3 +39,15 @@ class Yolo:
         nms_confs = [confs[int(i)].tolist() for i in filtered.tolist()]
         nms_clss = [clss[int(i)].tolist() for i in filtered.tolist()]
         return nms_boxes, nms_confs, nms_clss
+
+    def freeze_layer(self, trainer):
+        model = trainer.model
+        num_freeze = 10
+        print(f"Freezing {num_freeze} layers")
+        freeze = [f"model.{x}." for x in range(num_freeze)]  # layers to freeze
+        for k, v in model.named_parameters():
+            v.requires_grad = True  # train all layers
+            if any(x in k for x in freeze):
+                print(f"freezing {k}")
+                v.requires_grad = False
+        print(f"{num_freeze} layers are freezed.")

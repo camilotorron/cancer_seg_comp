0d6749096b03429c047e7cc0f5357890608ec90d
camilotorron <camilo_torron@hotmail.com>
2023-08-30 00:13:48 +0200

    evaluator class creation

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -1,8 +1,18 @@
 from PIL import Image
 import numpy as np
-from sklearn.metrics import jaccard_score as iou_score
-from sklearn.metrics import f1_score as f1
-from sklearn.metrics import adjusted_rand_score as rand_index_score
+
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import (
+    adjusted_rand_score,
+    jaccard_score,
+    f1_score,
+    precision_score,
+    recall_score,
+)
+from skimage.metrics import adapted_rand_error
+import matplotlib.pyplot as plt
+
+
 from typing import Union
 
 
@@ -11,19 +21,14 @@ class Evaluator:
     pred_mask = None
 
     def __init__(self, true_mask, pred_mask):
-        """
-        Initialize with two masks.
-
-        :param mask1: Path to the first mask image or PIL Image object
-        :param mask2: Path to the second mask image or PIL Image object
-        """
         # Load images if they are file paths
         if isinstance(true_mask, str):
             true_mask = Image.open(true_mask)
         if isinstance(pred_mask, str):
             pred_mask = Image.open(pred_mask)
-
         # Convert to numpy arrays
+        # self.create_comparison_image(true_mask=true_mask, pred_mask=pred_mask)
+        # breakpoint()
         self.true_mask = np.array(true_mask).flatten()
         self.pred_mask = np.array(pred_mask).flatten()
 
@@ -31,23 +36,89 @@ class Evaluator:
         """
         Compute Intersection over Union (IoU).
         """
-        return iou_score(y_true=self.true_mask, y_pred=self.pred_mask, average="micro")
+        if np.all(self.true_mask == self.pred_mask):
+            return 1.0
+        else:
+            return jaccard_score(
+                y_true=self.true_mask,
+                y_pred=self.pred_mask,
+                pos_label=255,
+                average="binary",
+                zero_division=0,
+            )
 
     def dice_coefficient(self):
         """
         Compute Dice Coefficient.
         """
-        intersection = np.sum(self.true_mask * self.pred_mask)
-        return (2.0 * intersection) / (np.sum(self.true_mask) + np.sum(self.pred_mask))
+        # intersection = np.sum(self.true_mask * self.pred_mask)
+        # return (2.0 * intersection) / (np.sum(self.true_mask) + np.sum(self.pred_mask))
+        # Compute the intersection
+        if np.all(self.true_mask == self.pred_mask):
+            return 1.0
+        else:
+            true_mask_norm = self.true_mask / 255
+            pred_mask_norm = self.pred_mask / 255
+            intersection = np.logical_and(
+                true_mask_norm,
+                pred_mask_norm,
+            ).sum()
+
+            # Compute the Dice Score
+            return (
+                2 * intersection / (true_mask_norm.sum() + pred_mask_norm.sum())
+                if intersection != 0
+                else 0
+            )
+
+    def precision(self):
+        TP = np.logical_and(self.true_mask, self.pred_mask).sum()
+        FP = np.logical_and(np.logical_not(self.true_mask), self.pred_mask).sum()
+        return TP / (TP + FP) if (TP + FP) != 0 else 0
+
+    def recall(self):
+        TP = np.logical_and(self.true_mask, self.pred_mask).sum()
+        FN = np.logical_and(self.true_mask, np.logical_not(self.pred_mask)).sum()
+        return TP / (TP + FN) if (TP + FN) != 0 else 0
 
     def f1_score(self):
         """
         Compute F1 Score.
         """
-        return f1(y_true=self.true_mask, y_pred=self.pred_mask, average="micro")
+        prec = self.precision()
+        recall = self.recall()
+        return (2 * (prec * recall) / (prec + recall)) if (prec + recall) != 0 else 0
 
-    def rand_index(self):
+    def compute_all_metrics(self):
+        return (
+            self.iou(),
+            self.dice_coefficient(),
+            self.f1_score(),
+            self.precision(),
+            self.recall(),
+        )
+
+    def create_comparison_image(self, true_mask, pred_mask, filename="image.png"):
         """
-        Compute Rand Index (RI).
+        Create an image comparing true_mask and pred_mask side by side.
+
+        Parameters:
+            true_mask (np.ndarray): The true mask as a 256x256 numpy array.
+            pred_mask (np.ndarray): The predicted mask as a 256x256 numpy array.
+            filename (str): The name of the output image file.
         """
-        return rand_index_score(labels_true=self.true_mask, labels_pred=self.pred_mask)
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+        # Plot true_mask
+        axes[0].imshow(true_mask, cmap="gray")
+        axes[0].set_title("True")
+        axes[0].axis("off")
+
+        # Plot pred_mask
+        axes[1].imshow(pred_mask, cmap="gray")
+        axes[1].set_title("Pred")
+        axes[1].axis("off")
+
+        # Save the image
+        plt.tight_layout()
+        plt.savefig(filename)

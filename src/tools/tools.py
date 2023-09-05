fc3b36bb1c21bcc815ed6036f84d64900dbe41f8
camilotorron <camilo_torron@hotmail.com>
2023-09-06 00:50:39 +0200

    moved methods to tools

--- src/tools/tools.py
+++ src/tools/tools.py
@@ -1,7 +1,8 @@
 import os
-from PIL import Image
+
 import numpy as np
 from loguru import logger
+from PIL import Image
 
 
 def get_png_files(path):
@@ -42,3 +43,73 @@ def check_is_tumor(mask_path: str) -> bool:
         return True
     except:
         logger.warning(f"Mask: {mask_path} if not readable.")
+
+
+def get_bounding_box(mask_path):
+    """
+    Computes the bounding box a mask
+
+    Args:
+        mask_path (_type_): path of the mask
+
+    Returns:
+        _type_: bbox in pixels
+    """
+    mask = Image.open(mask_path)
+    mask_np = np.array(mask)
+    binarized_array = (mask_np > 125).astype(int)
+    segmentation = np.where(binarized_array == True)
+
+    x_min, x_max, y_min, y_max = 0, 0, 0, 0
+    if len(segmentation) != 0 and len(segmentation[1]) != 0 and len(segmentation[0]) != 0:
+        x_min = int(np.min(segmentation[1]))
+        x_max = int(np.max(segmentation[1]))
+        y_min = int(np.min(segmentation[0]))
+        y_max = int(np.max(segmentation[0]))
+
+    bbox = x_min, x_max, y_min, y_max
+    return bbox
+
+
+def check_all_black_mask(mask_path: str) -> bool:
+    """
+    Check if a mask is all black
+
+    Args:
+        mask_path (str): path of the mask
+
+    Returns:
+        bool: True if all black
+    """
+    img = Image.open(mask_path).convert("L")
+    img_np = np.array(img)
+    return np.all(img_np == 0)
+
+
+def bbox_to_yoloformat(row):
+    """
+    Convert bbox in pixels to yolo format
+
+    Args:
+        row (_type_): row of the features df
+
+    Returns:
+        _type_: bbox in yolo format
+    """
+
+    bbox = row["bbox"]
+    imgsz = row["image_size"]
+    x_min, x_max, y_min, y_max = bbox[0], bbox[1], bbox[2], bbox[3]
+    x_center = (x_min + x_max) / 2
+    y_center = (y_min + y_max) / 2
+    width = x_max - x_min
+    height = y_max - y_min
+
+    x_center_n = x_center / imgsz[0]
+    y_center_n = y_center / imgsz[1]
+    width_n = width / imgsz[0]
+    height_n = height / imgsz[1]
+
+    yolo_bbox = (x_center_n, y_center_n, width_n, height_n)
+
+    return yolo_bbox

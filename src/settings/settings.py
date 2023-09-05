6e75416ba565d2d1d9b478fdea52bd12394a9db2
camilotorron <camilo_torron@hotmail.com>
2023-09-06 00:51:16 +0200

    updated

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,7 +1,8 @@
 import os
-from pydantic import BaseSettings
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -10,7 +11,7 @@ load_dotenv()
 class Environment(BaseSettings):
     IMAGES_PATH: str = os.getenv("IMAGES_PATH")
     BREAST_US: str = "breast_ultrasound_images_dataset/Dataset_BUSI_with_GT_proc"
-    YOLO_DATASET_OUTPUT: str = str(Path("data/yolo_ouput_dataset").resolve())
+    YOLO_DATASET_OUTPUT: str = str(Path("datasets/yolo_ouput_dataset").resolve())
     labels_dict = {
         "benign": 0,
         "malignant": 1,

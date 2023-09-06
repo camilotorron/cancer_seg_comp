232aba0f2a33ce6753498100c82312f960b57f3a
camilotorron <camilo_torron@hotmail.com>
2023-09-06 02:50:22 +0200

    train brain data

--- scripts/stages/train_yolo_det.py
+++ scripts/stages/train_yolo_det.py
@@ -2,11 +2,30 @@
     Description: 
         This script trains Yolov8 for object detection with all datasets available
 """
+import json
+
+from src.techniques.detection.yolo import Yolo
+
+# from src.data_handling.brain_data import BrainData
 
 
-def main():
-    pass
+def train(data: dict):
+    yolo = Yolo()
+    # braindata = BrainData(df_path=data.get("csv"))
+    yolo.train(data=data.get("yml"))
+    metrics = yolo.model.val()
+    return metrics
 
 
 if __name__ == "__main__":
-    main()
+    yolo_data = [
+        {"csv": "datasets/brain/base_df_yolodet.csv", "yml": "datasets/brain/base_det.yml"},
+        {"csv": "datasets/brain/augmented4_df_yolodet.csv", "yml": "datasets/brain/augmented4_det.yml"},
+        {"csv": "datasets/brain/augmented10_df_yolodet.csv", "yml": "datasets/brain/augmented10_det.yml"},
+    ]
+    metrics_list = []
+    for data in yolo_data:
+        metrics_list.append(train(data=data))
+    # Save to file
+    with open("datasets/brain/train_detect_result.json", "w") as f:
+        json.dump(data, f)
